1b598c39536fde7361c9c0e27692c8c8910c0ca0
Vokler <valerypavlikov@yandex.ru>
2021-05-11 21:34:17 +0300

    created basketapp application

--- authapp/views.py
+++ authapp/views.py
@@ -3,6 +3,7 @@ from django.contrib import auth, messages
 from django.urls import reverse
 
 from authapp.forms import UserLoginForm, UserRegisterForm, UserProfileForm
+from basketapp.models import Basket
 
 
 def login(request):
@@ -42,7 +43,11 @@ def profile(request):
             return HttpResponseRedirect(reverse('users:profile'))
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'GeekShop - Личный кабинет', 'form': form}
+    context = {
+        'title': 'GeekShop - Личный кабинет',
+        'form': form,
+        'baskets': Basket.objects.all(),
+    }
     return render(request, 'authapp/profile.html', context)
 
 
