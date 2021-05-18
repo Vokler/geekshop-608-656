a1574d188f6fb75d05259964a4da7a58a22e5098
Vokler <valerypavlikov@yandex.ru>
2021-05-18 20:48:55 +0300

    added update users

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from authapp.forms import UserRegisterForm
+from authapp.forms import UserRegisterForm, UserProfileForm
 from authapp.models import User
 
 
@@ -10,3 +10,8 @@ class UserAdminRegisterForm(UserRegisterForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'image', 'first_name', 'last_name', 'password1', 'password2')
+
+
+class UserAdminProfileForm(UserProfileForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control py-4'}))
